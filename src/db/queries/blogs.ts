1bf9fd824bfb506454f3574af6a77c6255997f1c
viraPentsak <61148110+viraPentsak@users.noreply.github.com>
2024-07-10 17:35:19 +0300

    Updates blogs query

--- src/db/queries/blogs.ts
+++ src/db/queries/blogs.ts
@@ -1,8 +1,11 @@
 import dbConnect from "@/db";
 import Blog, {I_Blog} from "@/models/Blog";
+import {Document} from "mongoose";
 
-export async function fetchBlogs(limit: number = 5): Promise<I_Blog[]>{
+export async function fetchBlogs(limit: number = 5): Promise<I_Blog[]> {
     await dbConnect();
-    const blogs = await Blog.find({}).limit(limit);
-    return blogs;
+    const blogs: Document<I_Blog>[] = await Blog.find({}).limit(limit);
+
+    // blogs
+    return blogs.map(blog => blog.toObject());
 }
