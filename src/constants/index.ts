232210eb66a54d6a9760f6de1abab92c682cc461
viraPentsak <61148110+viraPentsak@users.noreply.github.com>
2024-11-02 16:04:24 +0200

    Variable rename

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ import ERRORS from "./errors";
 import MESSAGES from "./messages";
 import PATHS from "./paths"
 
-const DB_URL = process.env.MONGO_DATABASE_URL;
+const DB_URL = process.env.MONGODB_URI;
 
 export {
     DB_URL,
